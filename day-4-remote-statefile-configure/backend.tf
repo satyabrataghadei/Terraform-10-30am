f5f04aaa5b221e83c5063551a91ef326a216d993
veera <veerababu.narni232@gmail.com>
2024-05-14 11:45:26 +0530

    updates

--- day-4-remote-statefile-configure/backend.tf
+++ day-4-remote-statefile-configure/backend.tf
@@ -1,7 +1,9 @@
 terraform {
   backend "s3" {
-    bucket = "state-remote-store"
-    key    = "/folder-2/terraform.tfstate"
+    bucket = "qwertyuioasdfghil"
+    key    = "terraform.tfstate"
     region = "ap-south-1"
+    dynamodb_table = "terraform-state-lock-dynamo" # DynamoDB table used for state locking, note: first run day-4-statefile-s3
+    encrypt        = true  # Ensures the state is encrypted at rest in S3.
   }
 }
--- day-4-remote-statefile-configure/main.tf
+++ day-4-remote-statefile-configure/main.tf
@@ -1,9 +1,9 @@
 provider "aws" {
-  region = "us-east-1"
+  
 }
 
 resource "aws_instance" "name" {
-    ami = "ami-013e83f579886baeb"
+    ami = "ami-0cc9838aa7ab1dce7"
     instance_type = "t2.micro"
     key_name = "jenkin"
 
