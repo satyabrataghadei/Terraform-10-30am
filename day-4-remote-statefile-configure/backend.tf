6ec9f9e930e7071425d2879dad1b09ad8777e64e
CloudTechDevOps <147053314+CloudTechDevOps@users.noreply.github.com>
2024-05-14 14:14:22 +0530

    Update backend.tf

--- day-4-remote-statefile-configure/backend.tf
+++ day-4-remote-statefile-configure/backend.tf
@@ -3,7 +3,7 @@ terraform {
     bucket = "statefile-configure"
     key    = "terraform.tfstate"
     region = "us-east-1"
-    dynamodb_table = "terraform-state-lock-dynamo" # DynamoDB table used for state locking, note: first run day-4-statefile-s3
+    dynamodb_table = "terraform-state-lock-dynamo" # DynamoDB table used for state locking, note: first run day-4-statefile-create=s3
     encrypt        = true  # Ensures the state is encrypted at rest in S3.
 
   }
